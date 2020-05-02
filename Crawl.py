67496da7837f2b4a2534c13e2a448b2a4fe7fc95
OGIceColed <cwestleyeverett@gmail.com>
2020-05-02 15:34:06 -0400

    Finished storing items into DB and started on pulling records that are sorted

--- Crawl.py
+++ Crawl.py
@@ -2,6 +2,8 @@ import wikipedia
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 
+# TODO: Find way to pull out x number of longest records in DB and insert into variable
+
 # Connect to local CentOS server running MariaDB
 server = SSHTunnelForwarder(
     '192.168.86.153',
@@ -23,32 +25,43 @@ cnx = pymysql.connect(
 
 cursor = cnx.cursor()
 
-# Get title of a random article on Wikipedia
+# Get title of a random article on Wikipedia. Pulls out first word of the title
 def getTitle():
     title = wikipedia.random(pages=1)
+    fullTitle = str.split(title)
+    title = fullTitle[0]
     return title
 
-# Get first sentence of a summary of the article in the getTitle function
+# Get first sentence of a summary of the article in the getTitle function. I may try to incorporate this at some point.
 def getSummary(title):
     summary = wikipedia.summary(title, sentences=1)
     return summary
 
-# TODO: Convert test DB insert into variables pulled from Wikipedia
+# Insert the first word of the title into the database.
+def queryTitle(title):
+    query = "INSERT INTO SiteContent(PageText) VALUES (%s)"
+    cursor.execute(query, (title))
+    cnx.commit()
+
+# Sort table by string length of the title and pulls out the largest record
+def sortTest():
+    query = "SELECT PageText FROM SiteContent ORDER BY CHAR_LENGTH(PageText) DESC;"
+    cursor.execute(query)
+    cnx.commit()
+    record = cursor.fetchone()
+    print(record)
+
+# Closes DB and SSH connections
+def closeConnection():
+    cursor.close()
+    cnx.close()
+    server.close()
+
 i = 1
 for i in range (1,5):
     title = getTitle()
     print(title)
-    words = str.split(title)
+    queryTitle(title)
 
-    summary = getSummary(title)
-    print(summary)
-    sumWords = str.split(summary)
-
-    print(words)
-    print(sumWords)
-
-cursor.execute("INSERT INTO SiteContent(PageText) VALUES ('Test')")
-cnx.commit()
-cursor.close()
-cnx.close()
-server.close()
+sortTest()
+closeConnection()
